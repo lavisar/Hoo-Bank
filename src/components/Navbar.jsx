7f862d0ac77d72a2362483eddbfb627d7bd04a0d
lavisar <sarlavi6969@gmail.com>
2023-12-15 14:58:35 +0700

    Add blur BG  when scroll navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
+import styles from '../style'
 import { close, logo, menu } from '../assets';
 import { navLinks } from '../constants';
 
 const Navbar = () => {
 	const [toggle, settoggle] = useState(false);
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > 0) {
+				setIsScrolled(true);
+			} else {
+				setIsScrolled(false);
+			}
+		};
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
 	return (
-		<nav className="w-full flex py-6 justify-between items-center navbar">
+		<nav
+			className={`sm:px-16 px-6 w-full flex py-6 justify-between items-center fixed top-0 ${
+				isScrolled ? 'z-10 backdrop-blur-md bg-black/30' : ''
+			}`}
+		>
 			<img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
 			<ul className="list-none sm:flex hidden justify-end items-center flex-1">
 				{navLinks.map((item, index) => (
 					<li
 						key={item.id}
-						className={`font-poppins font-normal cursor-pointer text-base text-white ${
+						className={`hover:text-secondary font-poppins font-normal cursor-pointer text-base text-white ${styles.hoverTranstion} ${
 							index === navLinks.length - 1 ? 'mr-0' : 'mr-10'
 						}`}
 					>
@@ -35,19 +55,21 @@ const Navbar = () => {
 						toggle ? 'flex' : 'hidden'
 					} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
 				>
-          <ul className="list-none flex flex-col justify-end items-center flex-1">
-				{navLinks.map((item, index) => (
-					<li
-						key={item.id}
-						className={`font-poppins font-normal cursor-pointer text-base text-white ${
-							index === navLinks.length - 1 ? 'mr-0' : 'mb-4'
-						}`}
-					>
-						<a href={`#${item.id}`}>{item.title}</a>
-					</li>
-				))}
-			</ul> 
-        </div>
+					<ul className="list-none flex flex-col justify-end items-center flex-1">
+						{navLinks.map((item, index) => (
+							<li
+								key={item.id}
+								className={`font-poppins font-normal cursor-pointer text-base text-white ${
+									index === navLinks.length - 1
+										? 'mr-0'
+										: 'mb-4'
+								}`}
+							>
+								<a href={`#${item.id}`}>{item.title}</a>
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
